b5b0a98b4c4221ed3aaae1981b97a9ae6d133a78
Nicholas Ng <nicholasykng@gmail.com>
2019-11-18 04:10:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,14 @@ class Student
       DB[:conn].execute(sql)
     end
     def self.drop_table
-      
+      sql = <<-SQL
+      DROP TABLE students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+      SQL
+      DB[:conn].execute(sql)
       end
 
 end
