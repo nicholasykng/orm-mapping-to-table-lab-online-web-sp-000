28165afad4dcc4a1616ba3c69ba50a2ac6bf4722
Nicholas Ng <nicholasykng@gmail.com>
2019-11-18 04:03:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,10 +7,10 @@ class Student
       @grade = grade
     end
     def self.create_table
-      sql =  <<-SQL 
+      sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS songs (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
+        id INTEGER PRIMARY KEY,
+        name TEXT,
         album TEXT
         )
         SQL
